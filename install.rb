28225e7be85fc75b43a7b5a1a2a2cf1dad037118
Alex Sharp <ajsharp@gmail.com>
2008-11-19 15:00:30 -0600

    fixed the filepath where install.rb looks for the css file

--- install.rb
+++ install.rb
@@ -2,4 +2,4 @@ require 'fileutils'
 
 slideshow_css = File.dirname(__FILE__) + '/../../../public/stylesheets/slideshow.css'
 
-FileUtils.cp(File.dirname(__FILE__) + '/public/stylesheets/datepicker.css', slideshow_css) unless File.exists?(slideshow_css)
+FileUtils.cp(File.dirname(__FILE__) + '/public/stylesheets/slideshow.css', slideshow_css) unless File.exists?(slideshow_css)
